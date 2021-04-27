ed95ec12cb34709b6c77922931fd5a093ee00059
HuangCongQing <1756260160@qq.com>
2021-04-27 10:58:19 +0800

    update

---
+++ 2open3D/txt/01o3d_load_txt.py
@@ -0,0 +1,9 @@
+'''
+Description: 
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2021-04-27 10:56:14
+LastEditTime: 2021-04-27 10:57:45
+FilePath: /Point-Clouds-Visualization/2open3D/txt/01o3d_load_txt.py
+'''
